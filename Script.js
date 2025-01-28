9d4f08c91737e6775fe80d3c9823c982b37e9e48
Lucymemo <165195337+Lucymemo@users.noreply.github.com>
2025-01-29 00:58:23 +0200

    Script.js

--- Script.js
+++ Script.js
@@ -1,50 +1,14 @@
 // الحصول على عنصر div لعرض الرسوم المتحركة
-const animationDiv = document.createElement('div');
-animationDiv.classList.add('animation');
-document.body.appendChild(animationDiv);
-
-// وظيفة لإنشاء قلب متحرك
-function createHeart() {
-    const heart = document.createElement('div');
-    heart.classList.add('heart');
-    animationDiv.appendChild(heart);
-
-    // إضافة حركة لل قلب
-    heart.style.animation = 'heartbeat 1s infinite alternate ease-in-out';
-}
+// ... باقي الكود
 
 // إنشاء قلب كل ثانيتين
 setInterval(createHeart, 2000);
 
-// إضافة أنماط CSS للرسوم المتحركة
-const style = document.createElement('style');
-style.textContent = `
-    @keyframes heartbeat {
-        0% {
-            transform: scale(1);
-        }
-        50% {
-            transform: scale(1.2);
-        }
-        100% {
-            transform: scale(1);
-        }
-    }
+// إنشاء نص "I love you" المتحرك
+function createLoveMessage() {
+    const loveMessage = document.createElement('div');
+    loveMessage.classList.add('love-message');
+    animationDiv.appendChild(loveMessage);
+}
 
-    .animation {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
-
-    .heart {
-        width: 50px;
-        height: 50px;
-        background-color: red;
-        border-radius: 50%;
-        position: absolute;
-        animation-fill-mode: both;
-    }
-`;
-document.head.appendChild(style);
+createLoveMessage();
